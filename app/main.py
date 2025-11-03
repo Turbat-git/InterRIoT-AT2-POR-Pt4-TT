8dcaf8c21639b071248c9eca8487fc5073afc072
Turbat Turkhuu <20136824@tafe.wa.edu.au>
2025-11-03 15:11:59 +0800

    feat(step-8): Encrypt, Decrypt -Sung to the tune of 'Sunrise, Sunset')

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from app.decrypt import Decryptor
+from app.encrypt import Encryptor
+
+
+class FileAdapter:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_data(self):
+        with open(self.filename, "rb") as file:
+            data = file.read()
+        return data
+
+
+if __name__ == '__main__':
+    plaintext_input = 'Trial from FileAdapter!'
+    plaintext_key = 'abacadabra'
+
+    file = FileAdapter('data/frame.png')
+
+    encryption_key = Encryptor(plaintext_key)
+    decryption_key = Decryptor(plaintext_key)
+
+    encrypted_data = encryption_key.encrypt(plaintext_input)
+    decrypted_data = decryption_key.decrypt(encrypted_data)
+
+    print(encrypted_data)
+    print(decrypted_data)
+    
