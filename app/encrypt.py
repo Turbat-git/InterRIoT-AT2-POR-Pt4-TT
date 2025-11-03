1c25644afff5311648bea8c78b84a07e5c4c728c
Turbat Turkhuu <20136824@tafe.wa.edu.au>
2025-11-03 14:22:40 +0800

    feat(step-5): Encrypt it!

--- app/encrypt.py
+++ app/encrypt.py
@@ -1,5 +1,5 @@
 # Import cryptography library
-
+import cryptography
 from app.key_utils import key32, key64
 
 
@@ -9,6 +9,7 @@ class Encryptor:
         key64_ = key64(key32_)
 
         # We need to do something with the key...
+        self.key = key64_
 
     def _encrypt(self, data: bytes) -> bytes:
         """
